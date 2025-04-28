5124898e667c4bf1728831c90662649bffd18123
Riyane Bouakaz <riyane.bouakaz@etu.cyu.fr>
2025-04-28 10:54:10 +0200

    Update PieceGestion.c
    ajout des fonction :
    - Affiche_Matrice_Dynamique
    - Taille_Piece
    - Transformation_Piece
    - Free_Piece
    modification des fonction :
    - AfficheMatrice
    - main

--- PieceGestion.c
+++ PieceGestion.c
@@ -63,14 +63,25 @@ void ChargementPiece(FILE* f, char liste_matrice[NB_PIECE][TAILLE_PIECE][TAILLE_
     }
 }
 
-void AfficheMatrice(char piece[TAILLE_PIECE][TAILLE_PIECE]){
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
+void AfficheMatrice(char piece[TAILLE_PIECE][TAILLE_PIECE],int longueur,int hauteur){
+    for(int i=0;i<hauteur;i++){
+        for(int j=0;j<longueur;j++){
             printf("%c",piece[i][j]);
         }
         printf("\n");
     }
 }
+
+void Affiche_Matrice_Dynamique(char** piece,int longueur,int hauteur){
+    for(int i=0;i<hauteur;i++){
+        for(int j=0;j<longueur;j++){
+            printf("%c",piece[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 
 void Rotation90(char piece[TAILLE_PIECE][TAILLE_PIECE],char piece_rota[TAILLE_PIECE][TAILLE_PIECE], int n){
     if(n==0){
@@ -95,6 +106,86 @@ void Rotation90(char piece[TAILLE_PIECE][TAILLE_PIECE],char piece_rota[TAILLE_PI
     }
 }
 
+void Taille_Piece(char matrice[TAILLE_PIECE][TAILLE_PIECE],int* longueur, int* hauteur){
+    if(matrice == NULL){
+        printf("La matrice n'existe pas");
+        exit(1);
+    }
+    int cpt=0;
+    *longueur = 0;
+    *hauteur = 0;
+    for(int i=0;i<TAILLE_PIECE;i++){
+        cpt = 0;
+        for(int j=0;j<TAILLE_PIECE;j++){
+            if(matrice[i][j] == '@'){
+                cpt ++;
+            }
+        }
+        if(cpt > *longueur){
+            *longueur = cpt;
+        }
+    }
+    for(int z=0;z<TAILLE_PIECE;z++){
+        cpt = 0;
+        for(int y=0;y<TAILLE_PIECE;y++){
+            if(matrice[y][z] == '@'){
+                cpt ++;
+            }
+        }
+        if(cpt > *hauteur){
+            *hauteur = cpt;
+        }
+    }
+    
+}
+char** Transformation_Piece(char piece[TAILLE_PIECE][TAILLE_PIECE],int* longueur, int* hauteur){
+    int l,h,n=0;
+    Taille_Piece(piece,&l,&h);
+    char** tab = NULL;
+    tab = malloc(h*sizeof(char*));
+    if(tab == NULL){
+        printf("Erreur d'allocation");
+        exit(1);
+    }
+    for(int i=0;i<h;i++){
+        tab[i] = malloc(l*sizeof(char));
+        if(tab[i] == NULL){
+            printf("erreur d'allocation");
+            exit(1);
+        }
+    }
+    
+    int debut_l=TAILLE_PIECE,debut_h=TAILLE_PIECE;
+    for(int i=0;i<TAILLE_PIECE;i++){
+        for(int j=0;j<TAILLE_PIECE;j++){
+            if(piece[i][j] == '@'){
+                if(i<debut_h){
+                    debut_h = i;
+                }
+                if(j<debut_l){
+                    debut_l = j;
+                }
+            }
+        }
+    }
+    
+    for(int i=0;i<h;i++){
+        for(int j=0;j<l;j++){
+            tab[i][j] = piece[debut_h+i][debut_l+j];
+        }
+    }
+    *longueur = l;
+    *hauteur = h;
+    return tab;
+}
+
+void Free_Piece(char** piece,int hauteur){
+    for(int i=0;i<hauteur;i++){
+        free(piece[i]);
+    }
+    free(piece);
+}
+
 int main() {
     char tab[LIGNE][COLONNE];
     GrilleDepart(tab, LIGNE, COLONNE);
@@ -110,10 +201,16 @@ int main() {
     char pieces[NB_PIECE][TAILLE_PIECE][TAILLE_PIECE];
     ChargementPiece(f, pieces);
     fclose(f);
-    AfficheMatrice(pieces[1]);
+    AfficheMatrice(pieces[1],5,5);
     char rotation[5][5];
-    Rotation90(pieces[1],rotation,2);
+    Rotation90(pieces[1],rotation,1);
     printf("\n");
-    AfficheMatrice(rotation);
+    AfficheMatrice(rotation,5,5);
+    printf("\n\n");
+    char** t_piece = NULL;
+    int h,l;
+    t_piece = Transformation_Piece(rotation,&l,&h);
+    Affiche_Matrice_Dynamique(t_piece,l,h);
+    Free_Piece(t_piece,h);
     return 0;
 }
