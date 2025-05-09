5d5103b22580474aa87a1bd6e87f7aed8b7acbb0
Riyane Bouakaz <riyane.bouakaz@etu.cyu.fr>
2025-05-09 10:28:45 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
 #define COLONNE 21
 #define LIGNE 10
 #define NB_PIECE 7
 #define TAILLE_PIECE 5
+
+typedef enum{PARTIE=1,OPTION,SCORE,QUITTER}Choixmenu;
 
 typedef struct{
     char nom[21];
@@ -21,6 +24,7 @@ void Rotation90(char piece[TAILLE_PIECE][TAILLE_PIECE],char piece_rota[TAILLE_PI
 void Taille_Piece(char matrice[TAILLE_PIECE][TAILLE_PIECE],int* longueur, int* hauteur);
 char** Transformation_Piece(char piece[TAILLE_PIECE][TAILLE_PIECE],int* longueur, int* hauteur);
 void Free_Piece(char** piece,int hauteur);
+int tirer_piece(int dernier1, int dernier2);
 
 //fonction_jeu :
 
@@ -29,9 +33,11 @@ void AffichePiece(int n, char tab[NB_PIECE][TAILLE_PIECE][TAILLE_PIECE]);
 void GrilleDepart(char tab[LIGNE][COLONNE], int l, int c);
 void AfficheMatrice(char piece[TAILLE_PIECE][TAILLE_PIECE],int longueur,int hauteur);
 void Affiche_Matrice_Dynamique(char** piece,int longueur,int hauteur);
-void PoserPiece(char grille[LIGNE][COLONNE], char** piece,int longueur, int hauteur , int colonne);
+int PoserPiece(char grille[LIGNE][COLONNE], char** piece,int longueur, int hauteur , int colonne);
 void DecalerLigneVersBas(char grille[LIGNE][COLONNE], int ligne);
-void SupprimerLignesPleines(char grille[LIGNE][COLONNE]);
+int SupprimerLignesPleines(char grille[LIGNE][COLONNE]);
+void vider_buffer();
+void game(Joueur* joueur);
 
 //fonction_score :
 
@@ -43,4 +49,3 @@ void triRapide(Joueur* liste_joueur,int taille);
 void triRapideRec(Joueur* liste_joueur, int debut, int fin);
 void Croissant_a_Decroissant(Joueur* liste_joueur,int taille);
 void Afficher_Top5(Joueur* liste_joueur,int taille);
-
